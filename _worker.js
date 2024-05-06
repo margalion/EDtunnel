19e149ece35847444e0c884cdbeb1a5ec4cd37cb
margalion <155935407+margalion@users.noreply.github.com>
2024-05-07 00:41:35 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '64ed5c15-f411-4ceb-84aa-605ed9633181';
 
-const proxyIPs = ['101.255.4.189'];
+const proxyIPs = ['103.75.188.126'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
